55b70e3fb238c07d64c224200557440a78f4065e
luka-duretic <luka.duretic@fer.unizg.hr>
2024-10-22 16:58:52 +0200

    dodajemo folder front u granu frontend

---
+++ front/src/App.tsx
@@ -0,0 +1,15 @@
+import Navbar from "./components/Navbar"
+import './App.css';
+
+function App(){
+  return (
+    <>
+      <div className="w-full bg-black px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
+            <Navbar/>
+      </div>
+    </>
+  )
+  
+}
+
+export default App;
